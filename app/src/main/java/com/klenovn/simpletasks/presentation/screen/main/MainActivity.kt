93be11d32908cf63414b5301bcc18702872e79d6
Nikita Klenov <146123813+klenovn@users.noreply.github.com>
2024-09-04 01:53:57 +0300

    Initial commit

---
+++ app/src/main/java/com/klenovn/simpletasks/presentation/screen/main/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.klenovn.simpletasks.presentation.screen.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.klenovn.simpletasks.presentation.theme.SimpleTasksTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimpleTasksTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun GreetingPreview() {
+    SimpleTasksTheme {
+        Greeting("Android")
+    }
+}
